630a9e2c0c1410f7e1dc798db725c6c2192cb332
wangchuande <wangchuande1223@gmail.com>
2016-06-18 09:27:15 +0800

    update

--- sina/amazon_capcha.py
+++ sina/amazon_capcha.py
@@ -130,5 +130,6 @@ if __name__ == '__main__':
         exit('Usage: amazon_capcha url')
     if len(sys.argv) == 2:
         url = sys.argv[1]
-        binarized(url)
+        image_buffer = urllib.urlopen(url).read()
+        binarized(image_buffer)
 
