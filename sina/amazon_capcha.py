60a9890cd62be8ca275094e4bdd0bc33fae7ec63
wangchuande <wangchuande1223@gmail.com>
2016-06-18 09:57:02 +0800

    update

--- sina/amazon_capcha.py
+++ sina/amazon_capcha.py
@@ -29,22 +29,43 @@ def get_projection_x(image):
 
 #获取分割后的x轴坐标点
 #返回值为[起始位置, 长度] 的列表
+# def get_split_seq(projection_x):
+#     res = []
+#     for idx in xrange(len(projection_x) - 1):
+#         p1 = projection_x[idx]
+#         p2 = projection_x[idx + 1]
+#         if p1 == 1 and idx == 0:
+#             res.append([idx, 1])
+#         elif p1 == 0 and p2 == 0:
+#             continue
+#         elif p1 == 1 and p2 == 1:
+#             res[-1][1] += 1
+#         elif p1 == 0 and p2 == 1:
+#             res.append([idx + 1, 1])
+#         elif p1 == 1 and p2 == 0:
+#             continue
+#     return res
+
+
+#获取分割后的x轴坐标点
+#返回值为[起始位置, 长度] 的列表
 def get_split_seq(projection_x):
     res = []
-    for idx in xrange(len(projection_x) - 1):
-        p1 = projection_x[idx]
-        p2 = projection_x[idx + 1]
-        if p1 == 1 and idx == 0:
-            res.append([idx, 1])
-        elif p1 == 0 and p2 == 0:
+    length = len(projection_x)
+    for idx in xrange(length - 1):
+        if projection_x[idx] == 0:
             continue
-        elif p1 == 1 and p2 == 1:
-            res[-1][1] += 1
-        elif p1 == 0 and p2 == 1:
-            res.append([idx + 1, 1])
-        elif p1 == 1 and p2 == 0:
-            continue
+        else:
+            for idx2 in range(idx+1, length):
+                if projection_x[idx2] == 1:
+                    continue
+                else:
+                    weight = idx2 - idx-1
+                    res.append([idx,weight])
     return res
+
+
+
 
 #分割后的图片，x轴分割后，同时去掉y轴上线多余的空白
 def split_image(image, split_seq=None):
